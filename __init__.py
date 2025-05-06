89be6496fb8a53f95605b47171640dd8d0707f50
QueenPuxxi <136255531+QueenPuxxi@users.noreply.github.com>
2025-05-06 19:25:04 +0800

    更新Pixian抠图

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from .mask_frame_cafe import maskframecafe
 from .RGBA2RGB import RGBA2RGB
 from .get_pic_name import GPICNAME
 from .load_img_from_path_1by1 import load_images_from_the_path_one_by_one
+from .pixian_rmbg import PixianRMBG
 
 WEB_DIRECTORY = "./js"
 
@@ -9,5 +10,6 @@ NODE_CLASS_MAPPINGS = {
     "自定义蒙版外框☕️": maskframecafe,
     "RGBA转为RGB☕️": RGBA2RGB,
     "获取图片名称☕️": GPICNAME,
-    "从路径依次加载图片☕️": load_images_from_the_path_one_by_one
+    "从路径依次加载图片☕️": load_images_from_the_path_one_by_one,
+    "Pixian RMBG☕️": PixianRMBG
     }
