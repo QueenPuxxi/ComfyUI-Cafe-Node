056893165bb995318c02879196f9472842837ac5
QueenPuxxi <136255531+QueenPuxxi@users.noreply.github.com>
2025-07-15 14:10:52 +0800

    更新注册

--- __init__.py
+++ __init__.py
@@ -3,6 +3,8 @@ from .RGBA2RGB import RGBA2RGB
 from .get_pic_name import GPICNAME
 from .load_img_from_path_1by1 import load_images_from_the_path_one_by_one
 from .pixian_rmbg import PixianRMBG
+from .randomly_delete_noncore_prompt import RandomlyDeleteNoncorePrompt
+from .loadimg import LoadImg
 
 WEB_DIRECTORY = "./js"
 
@@ -11,5 +13,7 @@ NODE_CLASS_MAPPINGS = {
     "RGBA转为RGB☕️": RGBA2RGB,
     "获取图片名称☕️": GPICNAME,
     "从路径依次加载图片☕️": load_images_from_the_path_one_by_one,
-    "Pixian RMBG☕️": PixianRMBG
+    "Pixian RMBG☕️": PixianRMBG,
+    "随机删除非核心提示词☕️": RandomlyDeleteNoncorePrompt,
+    "加载图像（✅透明通道✅文件名）☕️": LoadImg
     }
